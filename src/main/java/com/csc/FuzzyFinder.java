8eb27e14f866d3aae79231792f5e0538a6dc336d
Owen Ceresa <co84635@mail.harpercollege.edu>
2024-10-24 20:59:32 -0500

    Fuzzy Search Complete

--- src/main/java/com/csc/FuzzyFinder.java
+++ src/main/java/com/csc/FuzzyFinder.java
@@ -1,8 +1,57 @@
 package com.csc;
 
+import java.util.ArrayList;
+
 public class FuzzyFinder {
-  // Your code goes here!
-  public static void main(String args[]) {
-    System.out.println("In addition to your search functions, you can write a main function if you wish!");
+  public static int binarySearch(ArrayList<Fuzzy> fuzzieStrings, String fuzzieToFind) {
+    int low = 0;
+    int high = fuzzieStrings.size() - 1;
+
+    while (low <= high){
+      int middlePosition = (low + high) / 2;
+      int fuzzieComparison = fuzzieToFind.compareTo(fuzzieStrings.get(middlePosition).color);
+
+      if (fuzzieComparison == 0) {
+        return middlePosition;
+      } else if (fuzzieComparison < 0) {
+        high = middlePosition - 1;
+      } else {
+        low = middlePosition + 1;
+      }
+    }
+
+    return -1;
   }
+
+  public static int linearSearch(ArrayList<Fuzzy> fuzzieStrings, String fuzzieToFind){
+    for (int i = 0; i < fuzzieStrings.size(); i++){
+      if(fuzzieStrings.get(i).color.equals(fuzzieToFind)){
+          return i;
+      }
+    }
+
+    return -1;
+  }
+
+
+  public static void main(String args[]) {
+  FuzzyListGenerator generator = new FuzzyListGenerator();
+  FuzzyFinder finder = new FuzzyFinder();
+
+  ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+  ArrayList<Fuzzy> randomFuzzies = generator.randomizedRainbowFuzzies();
+
+  String fuzzieToFind = "gold";
+
+  int testOne = finder.linearSearch(sortedFuzzies, fuzzieToFind);
+  int testTwo = finder.binarySearch(sortedFuzzies, fuzzieToFind);
+  int testThree = finder.linearSearch(randomFuzzies, fuzzieToFind);
+  int testFour = finder.binarySearch(randomFuzzies, fuzzieToFind);
+
+  System.out.println("Linear Search in sortedFuzzies: " + testOne);
+  System.out.println("Binary Search in sortedFuzzies: " + testTwo);
+  System.out.println("Linear Search in randomFuzzies: " + testThree);
+  System.out.println("Binary Search in randomFuzzies: " + testFour);
 }
+}
+
--- src/test/java/com/csc/TestFuzzyFinder.java
+++ src/test/java/com/csc/TestFuzzyFinder.java
@@ -1,6 +1,7 @@
 package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.ArrayList;
 
@@ -29,4 +30,74 @@ public class TestFuzzyFinder {
     ArrayList<Fuzzy> fuzzies = generator.sortedRainbowFuzzies();
     assertEquals("purple", fuzzies.getFirst().color);
   }
+
+    @Test
+    void testLinearSearchWithSortedFuzzies() {
+        ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+        int index = finder.linearSearch(sortedFuzzies, "gold");
+        assertNotEquals(-1, index); 
+    }
+
+    @Test
+    void testBinarySearchWithSortedFuzzies() {
+        ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+        int index = finder.binarySearch(sortedFuzzies, "gold");
+        assertNotEquals(-1, index); 
+    }
+
+    @Test
+    void testLinearSearchWithRandomFuzzies() {
+        ArrayList<Fuzzy> randomFuzzies = generator.randomizedRainbowFuzzies();
+        int index = finder.linearSearch(randomFuzzies, "gold");
+        assertNotEquals(-1, index); 
+    }
+
+    @Test
+    void testBinarySearchWithRandomFuzzies() {
+        ArrayList<Fuzzy> randomFuzzies = generator.randomizedRainbowFuzzies();
+        int index = finder.binarySearch(randomFuzzies, "gold");
+        assertEquals(-1, index); 
+    }
+
+    @Test
+    void testEmptyListForLinearSearch() {
+      ArrayList<Fuzzy> emptyList = new ArrayList<>();
+      int index = finder.linearSearch(emptyList, "gold");
+      assertEquals(-1, index);
+    }
+
+    @Test
+    void testEmptyListForBinarySearch() {
+      ArrayList<Fuzzy> emptyList = new ArrayList<>();
+      int index = finder.binarySearch(emptyList, "gold");
+      assertEquals(-1, index);
+    }
+
+    @Test
+    void testNonExistentWordInSortedListWithLinear(){
+      ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+      int index = finder.linearSearch(sortedFuzzies, "Foo");
+      assertEquals(-1, index);
+    }
+
+    @Test
+    void testNonExistentWordInRandomListWithLinear(){
+      ArrayList<Fuzzy> sortedFuzzies = generator.randomizedRainbowFuzzies();
+      int index = finder.linearSearch(sortedFuzzies, "Foo");
+      assertEquals(-1, index);
+    }
+
+    @Test
+    void testNonExistentWordInSortedListWithBinary(){
+      ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+      int index = finder.binarySearch(sortedFuzzies, "Foo");
+      assertEquals(-1, index);
+    }
+
+    @Test
+    void testNonExistentWordInRandomListWithBinary(){
+      ArrayList<Fuzzy> sortedFuzzies = generator.randomizedRainbowFuzzies();
+      int index = finder.binarySearch(sortedFuzzies, "Foo");
+      assertEquals(-1, index);
+    }
 }
